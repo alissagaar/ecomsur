5d1bff9760bbe483c55ffc41fa503014f34e1ea8
Alissa Gamarra <alissa.gamarra@gmail.com>
2023-03-08 12:25:02 -0500

    estilos de CardA

--- front/src/Components/CardA.jsx
+++ front/src/Components/CardA.jsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar, faStarHalfStroke, faCartShopping} from '@fortawesome/free-solid-svg-icons'
 import { addToCart } from '../Redux/actions'
 import { useDispatch, useSelector } from 'react-redux'
+import s from './CardA.module.css'
 
 function CardA({id, name, image, brand, price, stock, rating, numReviews}) {
   const dispatch = useDispatch()
@@ -13,15 +14,15 @@ function CardA({id, name, image, brand, price, stock, rating, numReviews}) {
   const repeated = cart.find(product => product.id === id)
 
   return (
-    <div>
-      {/* <img src={`http://localhost:5000${image}`} alt={image} /> */}
+    <div className={s.cardA}>
+      <img src={`http://localhost:5000${image}`} alt={image} />
       <span>{brand}</span>
       <Link to={`/${id}`}><h3>{name}</h3></Link>
-      <div>
+      <div className={s.rating}>
         {rating===5? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></h4>: rating=== 4.5? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStarHalfStroke} /></h4> : rating=== 4? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></h4> : rating===3.5? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStarHalfStroke} /></h4> : rating===3? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></h4>: rating===2.5? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStarHalfStroke} /></h4> :rating===2? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></h4>: rating===1.5? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStarHalfStroke} /></h4>: <h4><FontAwesomeIcon icon={faStar} /></h4>}
-        <small>{numReviews}</small>
+        <div>{numReviews}</div>
       </div>
-      <div>
+      <div className={s.cta}>
         <h4>USD {price}</h4>
         {stock===0 ? 
         <button disabled><FontAwesomeIcon icon={faCartShopping} />&nbsp;&nbsp;ADD TO CART</button> : 
