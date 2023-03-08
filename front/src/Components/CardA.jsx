67b85850a432d1f8eaf4c9153c5af0a22f9f1740
Alissa Gamarra <alissa.gamarra@gmail.com>
2023-03-07 19:07:22 -0500

    Maquetación de CardA

--- front/src/Components/CardA.jsx
+++ front/src/Components/CardA.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar, faStarHalfStroke, faCartShopping} from '@fortawesome/free-solid-svg-icons'
 
 function CardA({id, name, image, brand, price, stock, rating, numReviews}) {
-  // const starsRating = rating===5? '★★★★★': rating===4.5? '★★★★'
   return (
     <div>
       {/* <img src={`http://localhost:5000${image}`} alt={image} /> */}
       <span>{brand}</span>
       <h3>{name}</h3>
-      <h4>{}</h4>
-      
-      <h4>{price}</h4>
-      {stock===0 ? <button disabled>Add item to cart</button> : <button>Add item to cart</button>}
+      <div>
+        {rating===5? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></h4>: rating=== 4.5? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStarHalfStroke} /></h4> : rating=== 4? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></h4> : rating===3.5? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStarHalfStroke} /></h4> : rating===3? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></h4>: rating===2.5? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStarHalfStroke} /></h4> :rating===2? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></h4>: rating===1.5? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStarHalfStroke} /></h4>: <h4><FontAwesomeIcon icon={faStar} /></h4>}
+        <small>{numReviews}</small>
+      </div>
+      <div>
+        <h4>USD {price}</h4>
+        {stock===0 ? 
+        <button disabled><FontAwesomeIcon icon={faCartShopping} /></button> : <button><FontAwesomeIcon icon={faCartShopping} /></button>}
+      </div>
     </div>
   )
 }
