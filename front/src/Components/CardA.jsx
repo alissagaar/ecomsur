87d92f50e115b4f5367443c251ed34ee06be5cd1
Alissa Gamarra <alissa.gamarra@gmail.com>
2023-03-07 19:37:46 -0500

    Linkeado con su página específica

--- front/src/Components/CardA.jsx
+++ front/src/Components/CardA.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar, faStarHalfStroke, faCartShopping} from '@fortawesome/free-solid-svg-icons'
 
@@ -7,7 +8,7 @@ function CardA({id, name, image, brand, price, stock, rating, numReviews}) {
     <div>
       {/* <img src={`http://localhost:5000${image}`} alt={image} /> */}
       <span>{brand}</span>
-      <h3>{name}</h3>
+      <Link to={`/${id}`}><h3>{name}</h3></Link>
       <div>
         {rating===5? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></h4>: rating=== 4.5? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStarHalfStroke} /></h4> : rating=== 4? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></h4> : rating===3.5? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStarHalfStroke} /></h4> : rating===3? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></h4>: rating===2.5? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStarHalfStroke} /></h4> :rating===2? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></h4>: rating===1.5? <h4> <FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStarHalfStroke} /></h4>: <h4><FontAwesomeIcon icon={faStar} /></h4>}
         <small>{numReviews}</small>
