08edca50e5f5d05d69f3849af9aa72e0bf7702c1
Alissa Gamarra <alissa.gamarra@gmail.com>
2023-03-07 20:52:58 -0500

    Dynamic NavBar with info of number of items in the cart

--- front/src/Components/NavBar.jsx
+++ front/src/Components/NavBar.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping} from '@fortawesome/free-solid-svg-icons'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
+  const cart = useSelector(state => state.cart)
   return (
     <nav>
-      <h2>LOGO</h2>
-      <h2>Cart(0)</h2>
+      <Link to={'/'}><h2>LOGO</h2></Link>
+      <Link to={'/cart'}><h2><FontAwesomeIcon icon={faCartShopping} />&nbsp;Cart({cart.length})</h2></Link>
     </nav>
   )
 }
