b3aaed2dee48c629743a43c5e554ed1292518c44
Alissa Gamarra <alissa.gamarra@gmail.com>
2023-03-07 19:38:19 -0500

    Maquetado de CardB, botones de stock y eliminar

--- front/src/Components/CardB.jsx
+++ front/src/Components/CardB.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark} from '@fortawesome/free-solid-svg-icons'
 
-function CardB() {
+function CardB({id, name, image, brand, price, stock}) {
+  const [quantity, setQuantity] = useState(1)
   return (
-    <div>CardB</div>
+    <div>
+      <img src={`http://localhost:5000${image}`} alt={image} />
+      <div>
+        <h5>{name}</h5>
+        <h4>USD {price} </h4>
+        <div>
+          {quantity===1 ? <button disabled>-</button>:<button onClick={() => setQuantity(prev => prev - 1)}>-</button>}
+          <span>{quantity}</span>
+          {quantity>= parseInt(stock) ?<button disabled>+</button>:<button onClick={() => setQuantity(prev => prev + 1)}>+</button>}
+        </div>
+      </div>
+      <div>
+        <button><FontAwesomeIcon icon={faXmark} /></button>
+      </div>
+    </div>
   )
 }
 
