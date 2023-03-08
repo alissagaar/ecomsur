0d46c09b67274ce6c5119490d4d47f08569d5e77
Alissa Gamarra <alissa.gamarra@gmail.com>
2023-03-08 12:25:42 -0500

    estilos de navbar

--- front/src/Components/NavBar.jsx
+++ front/src/Components/NavBar.jsx
@@ -3,13 +3,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping} from '@fortawesome/free-solid-svg-icons'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import s from './NavBar.module.css'
 
 function NavBar() {
   const cart = useSelector(state => state.cart)
   return (
-    <nav>
-      <Link to={'/'}><h2>LOGO</h2></Link>
-      <Link to={'/cart'}><h2><FontAwesomeIcon icon={faCartShopping} />&nbsp;Cart({cart.length})</h2></Link>
+    <nav className={s.nav}>
+      <Link to={'/'}><h2>NOVA</h2></Link>
+      <Link to={'/cart'}><h4><FontAwesomeIcon icon={faCartShopping} />&nbsp;Cart({cart.length})</h4></Link>
     </nav>
   )
 }
